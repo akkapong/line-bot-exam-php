f122c7b815618e41bdfad6b05d3c8df377020f8e
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2018-07-06 00:04:31 +0700

    test

--- webhooks.php
+++ webhooks.php
@@ -30,30 +30,34 @@ if (!is_null($events['events'])) {
 			// 	sendMessage($event['message']['text']+'!!!!!', $replyToken);
 			// }
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => json_encode($event)
-			];
+			if (($sender == $master) && ($event['message']['text'] == "ดันเต็ม!!"))
+			{
+				$messages = [
+					'type' => 'text',
+					'text' => "ดันเต็มแล้วครับ U64d87d61ed0b5946e739560a6b4c7675 @SW FURNITURE"
+				];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+				// Make a POST Request to Messaging API to reply to sender
+				$url = 'https://api.line.me/v2/bot/message/reply';
+				$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+				$post = json_encode($data);
+				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+				$ch = curl_init($url);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+				$result = curl_exec($ch);
+				curl_close($ch);
 
-			echo $result . "\r\n";
+				echo $result . "\r\n";
+			}
+			
 		}
 	}
 }
