ef67a3255a84f98ed62f5bdaaafed0ba5eae8e3d
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2018-07-06 16:55:39 +0700

    ccc

--- MyBot.php
+++ MyBot.php
@@ -1,16 +1,15 @@
 <?php
-require "vendor/autoload.php";
 
 class MyBot() {
 
-	private $access_token = 'FetFmZCPdNnWyowKNXIwZlFnZd8F3OK8O4h0JxLtZgvjOaAvhXNG9oe8iZwhyK5IdQdZzkXTJdW7hT+665l9Ud5JRIsMbs/kY10BD1dfi+v7YoFNlxAjeWauV5TivF/EfzEprLEI/ynUOhgkn2SKgAdB04t89/1O/w1cDnyilFU=';
+	private $access_token  = 'FetFmZCPdNnWyowKNXIwZlFnZd8F3OK8O4h0JxLtZgvjOaAvhXNG9oe8iZwhyK5IdQdZzkXTJdW7hT+665l9Ud5JRIsMbs/kY10BD1dfi+v7YoFNlxAjeWauV5TivF/EfzEprLEI/ynUOhgkn2SKgAdB04t89/1O/w1cDnyilFU=';
 	private $channelSecret = '166fe404020820ee2de356dc939170a6';
-
-	private $bot;
+	
+	private $url           = "https://api.line.me/v2/bot";
+	private $headers       = [];
 
 	function __construct(){
-		$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($this->access_token);
-		$this->bot  = new \LINE\LINEBot($httpClient, ['channelSecret' => $this->channelSecret]);
+		$this->headers = ['Authorization: Bearer ' . $access_token];
 	}
 
 	public function pushMessage($pushID, $message) {
--- testpush.php
+++ testpush.php
@@ -1,8 +1,11 @@
 <?php
+require "vendor/autoload.php";
+$access_token = 'FetFmZCPdNnWyowKNXIwZlFnZd8F3OK8O4h0JxLtZgvjOaAvhXNG9oe8iZwhyK5IdQdZzkXTJdW7hT+665l9Ud5JRIsMbs/kY10BD1dfi+v7YoFNlxAjeWauV5TivF/EfzEprLEI/ynUOhgkn2SKgAdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '166fe404020820ee2de356dc939170a6';
+$idPush = 'U451fc85ea12260354a24d5c20e035b09'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
 
-require "MyBot.php";
-
-echo "Tes Push...\n";
-
-$bot = new MyBot()
-$bot->manageTexMessage("push", ["U451fc85ea12260354a24d5c20e035b09"], "Test");
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
