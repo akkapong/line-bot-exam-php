7df8ef1d18684870f6b8a0c482c7c62e027eaf60
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2018-07-06 18:10:45 +0700

    test

--- testpush.php
+++ testpush.php
@@ -3,6 +3,13 @@ require "vendor/autoload.php";
 require "MyBot.php";
 // $access_token = 'FetFmZCPdNnWyowKNXIwZlFnZd8F3OK8O4h0JxLtZgvjOaAvhXNG9oe8iZwhyK5IdQdZzkXTJdW7hT+665l9Ud5JRIsMbs/kY10BD1dfi+v7YoFNlxAjeWauV5TivF/EfzEprLEI/ynUOhgkn2SKgAdB04t89/1O/w1cDnyilFU=';
 // $channelSecret = '166fe404020820ee2de356dc939170a6';
+
+//JAY
+//U6ea648a8d48e459bb7289b5fc6e7a477
+
+$content = file_get_contents('php://input');
+print_r($content);
+
 $idPush = 'U451fc85ea12260354a24d5c20e035b09';
 
 // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
@@ -15,7 +22,7 @@ $idPush = 'U451fc85ea12260354a24d5c20e035b09';
 // echo "\n===================\n";
 
 $bot = new MyBot();
-$res = $bot->manageTexMessage("push", [$idPush], "Test 1");
+// $res = $bot->manageTexMessage("push", [$idPush], "Test 1");
 
 // echo "\n===================\n";
 
