41a70623b767bab7581e40a01b1e54eb90a675c4
Akkapong Kajornwongwattana <akk.ohm@gmail.com>
2018-07-06 16:31:41 +0700

    cccc

--- webhooks.php
+++ webhooks.php
@@ -1,11 +1,9 @@
 <?php // callback.php
 
 require "vendor/autoload.php";
-require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+require "MyBot.php";
 
-$access_token = 'FetFmZCPdNnWyowKNXIwZlFnZd8F3OK8O4h0JxLtZgvjOaAvhXNG9oe8iZwhyK5IdQdZzkXTJdW7hT+665l9Ud5JRIsMbs/kY10BD1dfi+v7YoFNlxAjeWauV5TivF/EfzEprLEI/ynUOhgkn2SKgAdB04t89/1O/w1cDnyilFU=';
-
-$master = 'U451fc85ea12260354a24d5c20e035b09';
+$list = ["U451fc85ea12260354a24d5c20e035b09"];
 
 // Get POST body content
 $content = file_get_contents('php://input');
@@ -13,24 +11,34 @@ $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 function getMessage($event) {
-	$mode = "reply";
+	$mode    = "reply";
+	
+	$message = "Reply : ".$event['message']['text'];
+	
+	$command = $event['message']['text']
 
-	$messages = [
-		'type' => 'text',
-		'text' => "Reply : ".$event['message']['text']
-	];
-
-	if ($event['message']['text'] == "[hello]") {
-		$messages['text'] = "Hello!!";
-	} else if ($event['message']['text'] == "[full]") {
-		$mode             = "push";
-		$messages['text'] = "Test push";
-	} else {
-		$mode = "none";
+	switch ($command) {
+		case "[hello]":
+			$message = "Hello!!";
+			break;
+		case "[WAI]":
+			$message = "I am a bot!!";
+			break;
+		case "[userid]":
+			$message = "Your ID: ".$event['source']['userId'];
+			break;
+		case "[full]":
+			$mode    = "push";
+			$message = "Test push";
+			break;
+		default:
+			$mode = "none";
+			break;
 	}
 
-	return [$mode, $messages];
+	return [$mode, $message];
 }
+
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -38,40 +46,25 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$sender = $event['source']['userId'];
-
+			$sender     = $event['source']['userId'];
+			
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			//get message and mode
-			list($mode, $messages) = getMessage($event);
+			list($mode, $message) = getMessage($event);
 
 			//skip if don't know command
-			// if ($mode == "none") continue;
+			if ($mode == "none") continue;
+
+			$bot      = new MyBot();
+			$recevier = [$replyToken];
 			
-			// if (!$messages) continue;
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages'   => [$messages],
-			];
-			$post    = json_encode($data);
-			$headers = ['Content-Type: application/json', 'Authorization: Bearer ' . $access_token];
+			if ($mode == "push") {
+				$recevier = $list;
+			}
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-
-
-
+			echo $bot->manageTexMessage($mode, $recevier, $message);
 		}
 	}
 }
